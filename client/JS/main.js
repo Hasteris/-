bf803592e58f6eebcbe5b9f9e8cd163fc50d9762
210-14\210-14 <210>
2023-03-25 16:55:21 +0500

    создали сцену и объект

---
+++ client/JS/main.js
@@ -0,0 +1,11 @@
+const width = 1500
+const hight = 1500
+
+const canvas = document.querySelector("canvas")
+canvas.width = width
+canvas.hight = hight
+
+const ctx = canvas.getContext("2d")
+ctx.fillStyle = "hsla (0. 0%, 25%, 0.25)";
+
+const scene = document.querySelector(".scene")
